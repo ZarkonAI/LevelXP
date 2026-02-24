41ee2a84fbe7f3d7a300cc117a728dd359b12405
VoiD <tolya901003@gmail.com>
2026-02-24 10:57:05 +0500

    Refine D2 quick log modes with rest pattern support

---
+++ app/states.py
@@ -0,0 +1,14 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class QuickLogStates(StatesGroup):
+    choose_mode = State()
+    choose_exercise = State()
+    custom_name = State()
+    custom_primary_muscle = State()
+    enter_weight = State()
+    enter_reps = State()
+    enter_sets = State()
+    enter_rest_single = State()
+    enter_rest_pattern = State()
+    confirm = State()
