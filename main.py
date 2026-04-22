4063e24df7b0707da4e92177c2e600fdf83d38d5
VoiD <tolya901003@gmail.com>
2026-04-23 01:01:41 +0500

    patch 1.3(for host)

---
+++ main.py
@@ -0,0 +1,8 @@
+import asyncio
+import sys
+from app.main import main
+
+if __name__ == "__main__":
+    if sys.platform.startswith("win"):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
